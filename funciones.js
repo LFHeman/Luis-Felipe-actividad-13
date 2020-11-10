03005389c0a56e19995517d8e34e16331630f356
luisfelipegs <luisfelipe14@live.com.mx>
2020-11-10 11:36:41 -0600

    arreglos en js

---
+++ funciones.js
@@ -0,0 +1,23 @@
+const amigos []
+while(true){
+    let nombre=prompt(jesus)
+    if (nombre !== "fin"){
+    amigos.push(nombre)
+}else{
+    break;
+}
+}
+console.table (amigos;)
+
+const amigos =[]
+while(true){          
+     let nombre=prompt("Ingresa el nombre de tu amigo o amiga")
+
+     if (nombre !== "fin"){
+          amigos.push(nombre)
+     }else{
+          break;
+     }
+}
+
+console.table(amigos)
